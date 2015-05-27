5a243b0e09afc2dc1485222f052d246311b5c8e2
山崎孝洋 <tak_3_zaki3@yahoo.co.jp>
2015-05-28 00:40:27 +0900

    usermailerの定義

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -334,6 +334,7 @@ Rails.application.config.sorcery.configure do |config|
     # Default: `nil`
     #
     # user.reset_password_mailer =
+    user.reset_password_mailer = UserMailer
 
 
     # reset password email method on your mailer class.
