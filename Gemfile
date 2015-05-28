b24484aecf5f6b3ebd9c4c09077f78ce0c56210c
山崎孝洋 <tak_3_zaki3@yahoo.co.jp>
2015-05-28 22:48:15 +0900

    グラフ

--- Gemfile
+++ Gemfile
@@ -62,3 +62,5 @@ group :production do
   gem 'pg'
   gem 'rails_12factor'
 end
+
+gem 'chartkick'
--- app/controllers/heya_controller.rb
+++ app/controllers/heya_controller.rb
@@ -4,8 +4,12 @@ class HeyaController < ApplicationController
   end
 
   def show
-    @datas = Heya.all
-    puts @datas
+    @datas = Heya.order(:date).limit(10)
+    date_start = Time.now.utc.beginning_of_day
+    date_end = Time.now.utc.end_of_day
+
+    records = Heya.where(date: date_start..date_end).order(:date)
+    @chart_data  = records.map{|x| [x.date, x.temperature] }
   end
 
   def create
