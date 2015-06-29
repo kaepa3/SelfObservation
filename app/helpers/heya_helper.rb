02de2913b277161e2e7ab0bd86011eb2b58e6fa2
山崎孝洋 <tak_3_zaki3@yahoo.co.jp>
2015-06-30 01:39:54 +0900

    もうデバッグやで

--- app/helpers/heya_helper.rb
+++ app/helpers/heya_helper.rb
@@ -5,7 +5,9 @@ module HeyaHelper
   def get_datetime(params)
     date = Time.zone.now
     date = Time.parse(params[:select_date]) unless params[:select_date].blank?
-
+    puts date.beginning_of_day
+    puts Time.zone.now
+    puts "============================"
     [date.beginning_of_day, date.end_of_day]
   end
 
