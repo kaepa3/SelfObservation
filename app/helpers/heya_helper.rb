4e1fdfbe34b0b0de205d1a88541bb5c36c45e31b
山崎孝洋 <tak_3_zaki3@yahoo.co.jp>
2015-06-30 02:28:59 +0900

    no message

--- app/helpers/heya_helper.rb
+++ app/helpers/heya_helper.rb
@@ -5,7 +5,8 @@ module HeyaHelper
   def get_datetime(params)
     date = Time.zone.now
     date = Time.parse(params[:select_date]) unless params[:select_date].blank?
-    [date.utc.beginning_of_day, date.utc.end_of_day]
+    puts date.beginning_of_day
+    [date.beginning_of_day, date.end_of_day]
   end
 
   def today_forecasts(id)
